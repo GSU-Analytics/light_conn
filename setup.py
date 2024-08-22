e85e60d524e5571f40111fb0cf3dfc0aefce05d5
Isaac Kerson <ikerson@gsu.edu>
2024-08-22 10:55:45 -0400

    bump version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lightoracle',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
@@ -14,10 +14,10 @@ setup(
     description='A lightweight Oracle database connection handler.',
     keywords='oracle database connection pandas',
     url='https://github.com/GSU-Analytics/lightoracle.git', 
-        classifiers=[
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.10',
 )
